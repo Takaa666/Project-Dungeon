8d00c5d22268bf43cb55f47e60fab98378128be9
Jeryan Eka Yuda <jejey09@gt.student.pens.ac.id>
2024-11-02 22:59:06 +0700

    Bahan dan Potion, Front desk dan Dapur

---
+++ Assets/Scrpts/Bahan.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Bahan", menuName = "Food Prep/Food Item")]
+public class Bahan : ScriptableObject
+{
+    public string itemName;
+    public Sprite icon;
+    public bool isCooked;
+}
